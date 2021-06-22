08c0b2daad96f822ea664af7e5cf76851506a846
KiwiGeek <jpenman@lcg.org>
2021-06-21 21:41:15 -0400

    EP0002 - Win32 Basics https://www.youtube.com/watch?v=lw1TT0W0IYo

--- Main.c
+++ Main.c
@@ -1,12 +1,91 @@
 #include <stdio.h>
 
-#include <windows.h>
+#pragma warning(push, 0)
+#pragma warning(disable: 4668 4711)
+#include <Windows.h>
+#pragma warning(pop)
 
-int main(int argc, char* argv[])
+LRESULT MainWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+#pragma warning(disable: 28251)
+int WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, PSTR CommandLine, INT CmdShow)
 {
-	OutputDebugStringA("Hello ASCII.\n");
-	
-	OutputDebugStringW(L"Hello Unicode.\n");
-	
+	UNREFERENCED_PARAMETER(PreviousInstance);
+	UNREFERENCED_PARAMETER(CommandLine);
+	UNREFERENCED_PARAMETER(CmdShow);
+
+	WNDCLASSEXA WindowClass = { 0 };
+	HWND WindowHandle = 0;
+
+	WindowClass.cbSize = sizeof(WNDCLASSEXA);
+	WindowClass.style = 0;
+	WindowClass.lpfnWndProc = MainWindowProc;
+	WindowClass.cbClsExtra = 0;
+	WindowClass.cbWndExtra = 0;
+	WindowClass.hInstance = Instance;
+	WindowClass.hIcon = LoadIconA(NULL, IDI_APPLICATION);
+	WindowClass.hCursor = LoadCursorA(NULL, IDC_ARROW);
+	WindowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	WindowClass.lpszMenuName = NULL;
+	WindowClass.lpszClassName = "GAME_B_WINDOWCLASS";
+	WindowClass.hIconSm = LoadIconA(NULL, IDI_APPLICATION);
+
+	if (RegisterClassExA(&WindowClass) == 0)
+	{
+		MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+		return(0);
+	}
+
+	WindowHandle = CreateWindowExA(WS_EX_CLIENTEDGE, "GAME_B_WINDOWCLASS", "Window Title", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 240, 120, NULL, NULL, Instance, NULL);
+
+	if (WindowHandle == NULL)
+	{
+		MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+		return (0);
+	}
+
+	MSG Message = { 0 };
+
+	while (GetMessageA(&Message, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&Message);
+		DispatchMessageA(&Message);
+	}
+
+	return(0);
+}
+
+LRESULT CALLBACK MainWindowProc(
+	HWND hwnd,        // handle to window
+	UINT uMsg,        // message identifier
+	WPARAM wParam,    // first message parameter
+	LPARAM lParam)    // second message parameter
+{
+
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		// Initialize the window. 
+		return 0;
+
+	case WM_PAINT:
+		// Paint the window's client area. 
+		return 0;
+
+	case WM_SIZE:
+		// Set the size and position of the window. 
+		return 0;
+
+	case WM_DESTROY:
+		// Clean up window-specific data objects. 
+		return 0;
+
+		// 
+		// Process other messages. 
+		// 
+
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
 	return(0);
 }
